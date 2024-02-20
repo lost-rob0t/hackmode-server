b3c2f5a729d37d91d1b4d2853a9929ddebef2e60
nsaspy <nsaspy@fedora.email>
2024-02-20 02:23:08 -0500

    add files

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "project desc";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux =
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkg-config
+            sbcl
+            sbclPackages.wookie
+
+          ];
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath([pkgs.openssl])}
+          '';
+        };
+    };
+}
